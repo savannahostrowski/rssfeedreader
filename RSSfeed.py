439b7892bec7091cd44abd7b1ca3e942c2bac13f
Savannah Ostrowski <savannahostrowski@gmail.com>
2015-05-14 21:08:23 -0400

    rss feed reader

--- RSSfeed.py
+++ RSSfeed.py
@@ -1,21 +1,12 @@
-import feedparser, requests, urllib2
-from bs4 import BeautifulSoup
+import feedparser
 
 def readArticle():
 	postNum = 0
 	feed = feedparser.parse('http://www.cbc.ca/cmlink/rss-canada')
 	print (feed['feed']['title'])
 	for post in feed.entries:
-		print(str(postNum) + ') ' + post.title + ':' + post.link + '\n')
+		print(str(postNum) + ') ' + post.title + ': ' + post.link + '\n')
 		postNum += 1
-	whichArticle = eval(input('Based on the above links, what article number would you like to view: '))
-	article = feed.entries[whichArticle] 
-	link = article['link']
-	html = urllib2.urlopen(link).read()
-	soup = BeatifulSoup(html)
-	text = soup.findAll(text=True)
-	print(text)
-
 
 
 readArticle()
